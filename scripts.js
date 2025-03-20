801f6a01f8d55f3b4941a600fbc8df21d37e407f
blueglobesolutions <wendy@blueglobesolutions.com.au>
2025-03-20 13:10:39 +1100

    added hamburger menu for mobile

--- scripts.js
+++ scripts.js
@@ -1,11 +1,6 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.getElementById('hamburger');
-    const menu = document.getElementById('menu');
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('header ul');
 
-    hamburger.addEventListener('click', () => {
-        // Toggle 'active' class on the menu to open/close
-        menu.classList.toggle('active');
-        // Optionally, toggle the open class on hamburger for animation effect
-        hamburger.classList.toggle('open');
-    });
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('open');
 });
